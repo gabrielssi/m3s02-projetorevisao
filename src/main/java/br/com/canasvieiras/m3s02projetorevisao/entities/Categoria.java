cdf09dd80220d91686a212a14567ca855b0c6fc1
Gabriel Souza <gabrielsouzadasilva30@gmail.com>
2023-05-10 21:30:13 -0300

    Mário é bom

---
+++ src/main/java/br/com/canasvieiras/m3s02projetorevisao/entities/Categoria.java
@@ -0,0 +1,27 @@
+package br.com.canasvieiras.m3s02projetorevisao.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.*;
+@Data
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Date criacao = new Date();
+
+    @Column(nullable = false)
+    private Date atualizacao = new Date();
+
+    @Column(length = 30, nullable = false)
+    private String nome;
+
+    private String descricao;
+
+
+}
---
+++ src/main/java/br/com/canasvieiras/m3s02projetorevisao/entities/Pedido.java
@@ -0,0 +1,4 @@
+package br.com.canasvieiras.m3s02projetorevisao.entities;
+
+public class Pedido {
+}
